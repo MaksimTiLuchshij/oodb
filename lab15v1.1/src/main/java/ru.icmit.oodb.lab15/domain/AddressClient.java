f3f1d334b8a3d92f3376f515ec7cafd2e4e70a71
MaksimTiLuchshij <71039197+MaksimTiLuchshij@users.noreply.github.com>
2021-05-03 23:08:56 +0300

    Create AddressClient.java

---
+++ lab15v1.1/src/main/java/ru.icmit.oodb.lab15/domain/AddressClient.java
@@ -0,0 +1,21 @@
+package ru.icmit.oodb.lab15.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class AddressClient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AddressClient")
+    @SequenceGenerator(name = "AddressClient", sequenceName = "addressClient_seq", allocationSize = 1)
+    private int id;
+
+    private String address;
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+}
